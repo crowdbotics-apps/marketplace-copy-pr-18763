84616532c9941d2a7d41d999d0fc8ff1dd53a0b1
Crowdbotics <team@crowdbotics.com>
2020-07-17 04:20:52 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,7 @@
+import { REGISTER_USERNAME, REGISTER_PASSWORD } from "react-native-dotenv"
+const register = axios.create({
+  baseURL: "https://app3.jackrabbitclass.com/regv2.asp?id=540191",
+  auth: { username: REGISTER_USERNAME, password: REGISTER_PASSWORD },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 export const apiService = {}
